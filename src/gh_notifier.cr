9242ce7f9a56b41a5765370278f0e61bcf9ceb3d
007lva <007.lva@gmail.com>
2018-03-31 16:54:58 +0200

    Simplify code using Crest::Resource

--- src/gh_notifier.cr
+++ src/gh_notifier.cr
@@ -6,34 +6,38 @@ include GhNotifier::Github
 
 BASE_URL = "https://api.github.com"
 
-credentials = { user: ENV["USER"], password: ENV["pwd"] }
+client = Crest::Resource.new(
+  BASE_URL,
+  user: ENV["USER"],
+  password: ENV["PASSWORD"]
+)
+
 default_repo = "Soluciones/emergia"
 repo = ENV.fetch("REPO", default_repo)
 
 if ARGV.any? && ARGV.first == "approved"
-  pull_requests_request = Crest::Request.new(:get, "#{BASE_URL}/repos/#{repo}/pulls", **credentials)
-  pull_requests = Array(PullRequest).from_json(pull_requests_request.execute.body)
+  pull_requests_response = client["/repos/#{repo}/pulls"].get
+  pull_requests = Array(PullRequest).from_json(pull_requests_response.body)
 
   pull_requests.each do |pull_request|
-    url = "#{BASE_URL}/repos/#{repo}/pulls/#{pull_request.number}/reviews"
-    pull_request_reviews_request = Crest::Request.new(:get, url, **credentials)
-    pull_request_reviews = Array(PullRequestReview).from_json(pull_request_reviews_request.execute.body)
+    pull_request_reviews_response = client["/repos/#{repo}/pulls/#{pull_request.number}/reviews"].get
+    pull_request_reviews = Array(PullRequestReview).from_json(pull_request_reviews_response.body)
     number_of_approved_reviews = pull_request_reviews.count { |pr| pr.approved? }
     next if number_of_approved_reviews < 2
     puts "#{pull_request.html_url} #{pull_request.user.login}"
   end
 else
-  notifications_request = Crest::Request.new(:get, "#{BASE_URL}/repos/#{repo}/notifications", **credentials)
-  notifications = Array(Notification).from_json(notifications_request.execute.body)
+  notifications_response = client["/repos/#{repo}/notifications"].get
+  notifications = Array(Notification).from_json(notifications_response.body)
   notifications.each do |notification|
     if comment_url = notification.subject.latest_comment_url
-      comment_request = Crest::Request.new(:get, comment_url, **credentials)
-      comment = Comment.from_json(comment_request.execute.body)
+      comment_request = client[comment_url.gsub(BASE_URL, "")].get
+      comment = Comment.from_json(comment_request.body)
       puts comment.html_url
     else
       pull_request_url = notification.subject.url
-      pull_request_request = Crest::Request.new(:get, pull_request_url, **credentials)
-      pull_request = PullRequest.from_json(pull_request_request.execute.body)
+      pull_request_response = client[pull_request_url.gsub(BASE_URL, "")].get
+      pull_request = PullRequest.from_json(pull_request_response.body)
       puts pull_request.html_url
     end
   end
